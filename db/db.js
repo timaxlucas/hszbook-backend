2ba4f210c2a6ddd200543d7ae85068e483b4d880
Maximilian Lucas <mail@maximilian-lucas.de>
2019-05-22 02:34:31 +0200

    check db connection on startup

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const { Pool, Client } = require('pg');
 const format = require('pg-format');
+const logger = require('../helpers/logger');
 const forEach = require('async-foreach').forEach;
 const connectionString = require('../config.json').connectionString;
 
@@ -7,6 +8,14 @@ const pool = new Pool({
   connectionString: connectionString,
 });
 
+
+pool.connect((err, client, release) => {
+  if (err) {
+    logger.error(err, { source: 'db-init' });
+    process.exit(1);
+  }
+  release();
+});
 
 async function uploadSchedule({ user, date, kid, link, data }) {
   await pool.query(`INSERT INTO dbo.schedule(data, date, kid, link, "user") VALUES ($1, $2, $3, $4, $5)`, [data, date, kid, link, user]);
