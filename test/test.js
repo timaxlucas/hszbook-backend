a9d8b7bb0dd7bf7bf35db147caf1ca3821a49d09
Maximilian Lucas <dev@maximilian-lucas.de>
2019-05-29 15:32:50 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
-
 const db = require('../db/db');
 
+
 describe('scheduleService', () => {
-  test('abc', () => {
+  test('abc', async() => {
+    require('../modules/schedule/schedule.service');
     expect(true).toBe(true);
-  });
+  }, 10 * 1000);
+});
+
+
+afterAll(() => {
+  db.close();
 });
